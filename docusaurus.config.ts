8d5ff1c97849c4dac707b76a4e3e16761260a53d
Griffin Cox <griffin@ezbot.ai>
2024-05-20 17:56:11 -0500

    Fix Cananonical Link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://ezbot.ai",
+  url: "https://docs.ezbot.ai",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
