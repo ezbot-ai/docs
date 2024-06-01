9a0d480a0dbabc4b3e545ffd0ef19fa5e9a99c13
Griffin Cox <griffin@ezbot.ai>
2024-05-31 20:22:02 -0500

    remove next

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,9 +37,7 @@ const config: Config = {
         docs: {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/ezbot-ai/docs/tree/main/", // TODO: Is this correct?
+          editUrl: "https://github.com/ezbot-ai/docs/tree/main/",
         },
         blog: false,
         theme: {
@@ -93,9 +91,9 @@ const config: Config = {
           label: "GitHub",
           position: "right",
         },
-        {
-          type: "docsVersionDropdown",
-        },
+        // {
+        //   type: "docsVersionDropdown", // Turn me on if we have multiple versions
+        // },
       ],
     },
     footer: {
