3d09a49c485d40d54c7212f023dae8e6f3328a89
Griffin Cox <146489353+griffin-ezbot@users.noreply.github.com>
2024-06-11 17:26:53 -0500

    Update Termly Embed Script (#31)

--- src/js/_termly.js
+++ src/js/_termly.js
@@ -2,11 +2,6 @@ import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 if (ExecutionEnvironment.canUseDOM) {
   const termlyScript = document.createElement("script");
-  termlyScript.src = `https://app.termly.io/embed.min.js`;
-  termlyScript.setAttribute("data-auto-block", "on");
-  termlyScript.setAttribute(
-    "data-website-uuid",
-    "ed3d4eec-6473-45df-b680-2f7f13fa106f"
-  );
+  termlyScript.src = `https://app.termly.io/resource-blocker/ed3d4eec-6473-45df-b680-2f7f13fa106f?autoBlock=on`;
   document.head.appendChild(termlyScript);
 }
